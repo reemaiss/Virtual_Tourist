59cad97f6f0deeed49d8c6c8c0a59dd1c28bd8de
Rema Ibrahim Alsuwailm <ralsuwailm@elm.sa>
2021-01-10 11:28:44 +0300

    Initial Commit

---
+++ Virtual_Tourist_Rema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual_Tourist_Rema
+//
+//  Created by Rema alsuwailm on 26/05/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Virtual_Tourist_RemaTests/Virtual_Tourist_RemaTests.swift
@@ -0,0 +1,33 @@
+//
+//  Virtual_Tourist_RemaTests.swift
+//  Virtual_Tourist_RemaTests
+//
+//  Created by Rema alsuwailm on 26/05/1442 AH.
+//
+
+import XCTest
+@testable import Virtual_Tourist_Rema
+
+class Virtual_Tourist_RemaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
